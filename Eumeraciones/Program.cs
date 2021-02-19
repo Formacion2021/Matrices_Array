55a7dd427d3f2b8acb1d163c008ec3c7349e171e
OscarRCursoPlexus <OscarRCursoPlexus@outlook.es>
2021-02-19 10:04:23 +0100

    Agregar archivos de proyecto.

---
+++ Eumeraciones/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Eumeraciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo [] nombre;
+            char[] animales = new char[4] {'L','O','B','O' };
+            int[] numeros = new int[10];
+            //variable para recorrer la matriz
+            int i;
+            //Recorremos la matriz animales
+            for (i = 0; i < 4; i++)
+            {
+                Console.Write(" {0} ",animales[i]);
+            }
+                    //Introducimos los elementos en la matriz
+                    Console.WriteLine("Introduawefzca 10 elementos en la matriz: ");
+            for (i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Elementos: {0}", i);
+                try
+                {
+                    numeros[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch(Exception Ex)       
+                {
+                    Console.WriteLine(Ex.Message);
+                }
+                Console.Write("\nLos elementos de la matriz son ");
+                
+            }
+            //Recorremos los elementos de la matriz
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", numeros[i]);
+            }
+            Console.Write("\n");
+
+        }
+    }
+}
